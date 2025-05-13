e2cfc75ec0724ddedc5f1af232d8e67e604549b4
Joseph Berger <airmanberger@gmail.com>
2025-05-13 11:21:44 -0500

    adding config file

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
 db_uri = 'sqlite:///mtdb.db'
 timeout = 5
+# Logging configuration
+log_level = "INFO"  # You can set this to DEBUG, INFO, WARNING, ERROR, CRITICAL
+log_file = "mt.log"
+log_to_terminal = False # Set to True to also output logs to the terminal
